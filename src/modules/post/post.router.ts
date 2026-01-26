1e64d6b7f22930e50b2810546fd208abec9df44e
Alamin Mustafa Rahim <lucky17112000@gmail.com>
2026-01-26 22:51:17 +0600

    reset

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction , Request , Response , Router } from 'express';
 import { PostController } from './post.controller';
-import {auth as betterAuth} from '../../lib/auth';  
+  
 import { auth, userRole } from '../../middlware/auth';
 // import { success } from 'better-auth';
 const router  = express.Router();
