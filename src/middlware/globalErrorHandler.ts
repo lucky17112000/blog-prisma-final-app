b75baf0014027cb11fd16d528fb9e065afd4ad70
Alamin Mustafa Rahim <lucky17112000@gmail.com>
2026-02-02 01:52:05 +0600

    prisma client validation error

--- src/middlware/globalErrorHandler.ts
+++ src/middlware/globalErrorHandler.ts
@@ -1,10 +1,19 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 // import { error } from "node:console";
 function errorHandler(err:any , req:Request , res:Response , next:NextFunction){
     // if(res.headersSent){
     //     return next(err);
     // }
-    res.status(500).json({message : 'Something went wrong!' , error : err.message});
-    res.json({message : 'Something went wrong!' , error : err.message});
+    let statusCode = 500;
+    let errorMessage = "Internal Server Error";
+    let errorDetails = err;
+    //prisma client validation error
+    if(err instanceof Prisma.PrismaClientValidationError){
+        statusCode =400;
+        errorMessage = "You provide incorrect field or missing field";
+        
+    }
+    res.status(statusCode).json({message : errorMessage , error : errorDetails});
 }
 export default errorHandler;
